bf89276d020116addd3ca9f78e1b6880b08f718f
calvin <hhaou@outlook.com>
2023-03-24 00:26:49 +0800

    protogen add

---
+++ protogen.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+gobin=$(go env GOBIN)
+
+if test -z $gobin ; then
+    gopath=$(go env GOPATH)
+    if test -z $gopath; then
+      gopath="$HOME/go"
+    fi
+    gobin="$gopath/bin"
+fi
+
+export PATH="$PATH:$gobin"
+
+protoc=$(which protoc)
+
+if test -z "$protoc"; then
+  printf "protoc not found"
+  exit 1
+fi
+
+gengo=$(which protoc-gen-go)
+if test -z "$gengo"; then
+  printf "gengo not found "
+  exit  1
+fi
+
+
+function getdir(){
+    for element in `ls $1`
+    do
+        dir_or_file=$1"/"$element
+        if [ -d $dir_or_file ]
+        then
+            getdir $dir_or_file
+        else
+            echo $1
+            echo  $dir_or_file
+            ##注意：
+                          #
+                          #1> 提取文件后缀名： ${file##*.}
+                          #
+                          #    ##是贪婪操作符，从左至右匹配，匹配到最右边的.号，移除包含.号的左边内容。
+                          #2> 是=，而且其两边有空格，如果没有空格，会报错
+                          #
+                          #3> 多加了x，是为了防止字符串为空时报错。
+
+            if [ "${dir_or_file##*.}"x = "proto"x ]; then
+              protoc  --proto_path=$1 --validate_out="lang=go:$1" --go_out=$1  --doc_out=./doc --doc_opt=html,index.html $dir_or_file
+            fi
+        fi
+    done
+}
+root_dir="./protos"
+getdir $root_dir
+
