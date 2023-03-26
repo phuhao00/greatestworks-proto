d55c975f8942253eacb22521e790329b5e5bd2e6
calvin <hhaou@outlook.com>
2023-03-26 10:07:18 +0800

    login adding

--- login/login.pb.validate.go
+++ login/login.pb.validate.go
@@ -526,3 +526,76 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = WorldEndPointInfoRspValidationError{}
+
+// Validate checks the field values on KickOutPlayer with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *KickOutPlayer) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for UserID
+
+	// no validation rules for KickOutReason
+
+	// no validation rules for Reason
+
+	// no validation rules for SvrId
+
+	return nil
+}
+
+// KickOutPlayerValidationError is the validation error returned by
+// KickOutPlayer.Validate if the designated constraints aren't met.
+type KickOutPlayerValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e KickOutPlayerValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e KickOutPlayerValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e KickOutPlayerValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e KickOutPlayerValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e KickOutPlayerValidationError) ErrorName() string { return "KickOutPlayerValidationError" }
+
+// Error satisfies the builtin error interface
+func (e KickOutPlayerValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sKickOutPlayer.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = KickOutPlayerValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = KickOutPlayerValidationError{}
