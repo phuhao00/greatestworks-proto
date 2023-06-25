ba9fb11d962e9b65993046daa3a53550e4877b49
calvin <hhaou@outlook.com>
2023-06-25 11:42:53 +0800

    ading

--- server_common/sever_common.pb.validate.go
+++ server_common/sever_common.pb.validate.go
@@ -1032,3 +1032,71 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = GatewayForwardPacketValidationError{}
+
+// Validate checks the field values on SCKick with the rules defined in the
+// proto definition for this message. If any rules are violated, an error is returned.
+func (m *SCKick) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Kick
+
+	// no validation rules for Contend
+
+	return nil
+}
+
+// SCKickValidationError is the validation error returned by SCKick.Validate if
+// the designated constraints aren't met.
+type SCKickValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SCKickValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SCKickValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SCKickValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SCKickValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SCKickValidationError) ErrorName() string { return "SCKickValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SCKickValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSCKick.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SCKickValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SCKickValidationError{}
