e9525e8ab0abf58c873c16e56b5f7e16c1b6686d
calvin <hhaou@outlook.com>
2023-06-25 14:42:23 +0800

    adding

--- server_common/sever_common.pb.go
+++ server_common/sever_common.pb.go
@@ -231,6 +231,56 @@ func (ServerCategory) EnumDescriptor() ([]byte, []int) {
 	return file_sever_common_proto_rawDescGZIP(), []int{3}
 }
 
+//服务注册状态
+type ServerRegisterStatus int32
+
+const (
+	ServerRegisterStatus_Fail    ServerRegisterStatus = 0
+	ServerRegisterStatus_OK      ServerRegisterStatus = 1
+	ServerRegisterStatus_Unknown ServerRegisterStatus = 2
+)
+
+// Enum value maps for ServerRegisterStatus.
+var (
+	ServerRegisterStatus_name = map[int32]string{
+		0: "Fail",
+		1: "OK",
+		2: "Unknown",
+	}
+	ServerRegisterStatus_value = map[string]int32{
+		"Fail":    0,
+		"OK":      1,
+		"Unknown": 2,
+	}
+)
+
+func (x ServerRegisterStatus) Enum() *ServerRegisterStatus {
+	p := new(ServerRegisterStatus)
+	*p = x
+	return p
+}
+
+func (x ServerRegisterStatus) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (ServerRegisterStatus) Descriptor() protoreflect.EnumDescriptor {
+	return file_sever_common_proto_enumTypes[4].Descriptor()
+}
+
+func (ServerRegisterStatus) Type() protoreflect.EnumType {
+	return &file_sever_common_proto_enumTypes[4]
+}
+
+func (x ServerRegisterStatus) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use ServerRegisterStatus.Descriptor instead.
+func (ServerRegisterStatus) EnumDescriptor() ([]byte, []int) {
+	return file_sever_common_proto_rawDescGZIP(), []int{4}
+}
+
 type ActionData struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -1808,12 +1858,15 @@ var file_sever_common_proto_rawDesc = []byte{
 	0x66, 0x69, 0x6e, 0x65, 0x64, 0x10, 0x00, 0x12, 0x09, 0x0a, 0x05, 0x4c, 0x6f, 0x67, 0x69, 0x6e,
 	0x10, 0x01, 0x12, 0x0b, 0x0a, 0x07, 0x47, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x10, 0x02, 0x12,
 	0x09, 0x0a, 0x05, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x10, 0x03, 0x12, 0x06, 0x0a, 0x02, 0x44, 0x42,
-	0x10, 0x04, 0x42, 0x45, 0x5a, 0x43, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
-	0x2f, 0x70, 0x68, 0x75, 0x68, 0x61, 0x6f, 0x30, 0x30, 0x2f, 0x67, 0x72, 0x65, 0x61, 0x74, 0x65,
-	0x73, 0x74, 0x77, 0x6f, 0x72, 0x6b, 0x73, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x73, 0x65,
-	0x72, 0x76, 0x65, 0x72, 0x5f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x3b, 0x73, 0x65, 0x72, 0x76,
-	0x65, 0x72, 0x5f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+	0x10, 0x04, 0x2a, 0x35, 0x0a, 0x14, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x52, 0x65, 0x67, 0x69,
+	0x73, 0x74, 0x65, 0x72, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x08, 0x0a, 0x04, 0x46, 0x61,
+	0x69, 0x6c, 0x10, 0x00, 0x12, 0x06, 0x0a, 0x02, 0x4f, 0x4b, 0x10, 0x01, 0x12, 0x0b, 0x0a, 0x07,
+	0x55, 0x6e, 0x6b, 0x6e, 0x6f, 0x77, 0x6e, 0x10, 0x02, 0x42, 0x45, 0x5a, 0x43, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x70, 0x68, 0x75, 0x68, 0x61, 0x6f, 0x30, 0x30,
+	0x2f, 0x67, 0x72, 0x65, 0x61, 0x74, 0x65, 0x73, 0x74, 0x77, 0x6f, 0x72, 0x6b, 0x73, 0x2d, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f, 0x63, 0x6f, 0x6d, 0x6d,
+	0x6f, 0x6e, 0x3b, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -1828,30 +1881,31 @@ func file_sever_common_proto_rawDescGZIP() []byte {
 	return file_sever_common_proto_rawDescData
 }
 
-var file_sever_common_proto_enumTypes = make([]protoimpl.EnumInfo, 4)
+var file_sever_common_proto_enumTypes = make([]protoimpl.EnumInfo, 5)
 var file_sever_common_proto_msgTypes = make([]protoimpl.MessageInfo, 13)
 var file_sever_common_proto_goTypes = []interface{}{
 	(ActionParseType)(0),         // 0: ServerCommon.ActionParseType
 	(InterObjStatus)(0),          // 1: ServerCommon.InterObjStatus
 	(KickReason)(0),              // 2: ServerCommon.KickReason
 	(ServerCategory)(0),          // 3: ServerCommon.ServerCategory
-	(*ActionData)(nil),           // 4: ServerCommon.ActionData
-	(*PlayerBaseInfo)(nil),       // 5: ServerCommon.PlayerBaseInfo
-	(*Vec3)(nil),                 // 6: ServerCommon.Vec3
-	(*UnitInfo)(nil),             // 7: ServerCommon.UnitInfo
-	(*UnitActionData)(nil),       // 8: ServerCommon.UnitActionData
-	(*InteractiveObj)(nil),       // 9: ServerCommon.InteractiveObj
-	(*LimitInfo)(nil),            // 10: ServerCommon.LimitInfo
-	(*RegisterToGateway)(nil),    // 11: ServerCommon.RegisterToGateway
-	(*RegisterToGatewayRet)(nil), // 12: ServerCommon.RegisterToGatewayRet
-	(*UpdateOnlineInfo)(nil),     // 13: ServerCommon.UpdateOnlineInfo
-	(*UpdateOnlineInfoRet)(nil),  // 14: ServerCommon.UpdateOnlineInfoRet
-	(*GatewayForwardPacket)(nil), // 15: ServerCommon.GatewayForwardPacket
-	(*SCKick)(nil),               // 16: ServerCommon.SCKick
+	(ServerRegisterStatus)(0),    // 4: ServerCommon.ServerRegisterStatus
+	(*ActionData)(nil),           // 5: ServerCommon.ActionData
+	(*PlayerBaseInfo)(nil),       // 6: ServerCommon.PlayerBaseInfo
+	(*Vec3)(nil),                 // 7: ServerCommon.Vec3
+	(*UnitInfo)(nil),             // 8: ServerCommon.UnitInfo
+	(*UnitActionData)(nil),       // 9: ServerCommon.UnitActionData
+	(*InteractiveObj)(nil),       // 10: ServerCommon.InteractiveObj
+	(*LimitInfo)(nil),            // 11: ServerCommon.LimitInfo
+	(*RegisterToGateway)(nil),    // 12: ServerCommon.RegisterToGateway
+	(*RegisterToGatewayRet)(nil), // 13: ServerCommon.RegisterToGatewayRet
+	(*UpdateOnlineInfo)(nil),     // 14: ServerCommon.UpdateOnlineInfo
+	(*UpdateOnlineInfoRet)(nil),  // 15: ServerCommon.UpdateOnlineInfoRet
+	(*GatewayForwardPacket)(nil), // 16: ServerCommon.GatewayForwardPacket
+	(*SCKick)(nil),               // 17: ServerCommon.SCKick
 }
 var file_sever_common_proto_depIdxs = []int32{
-	4, // 0: ServerCommon.PlayerBaseInfo.actData:type_name -> ServerCommon.ActionData
-	8, // 1: ServerCommon.UnitInfo.actData:type_name -> ServerCommon.UnitActionData
+	5, // 0: ServerCommon.PlayerBaseInfo.actData:type_name -> ServerCommon.ActionData
+	9, // 1: ServerCommon.UnitInfo.actData:type_name -> ServerCommon.UnitActionData
 	0, // 2: ServerCommon.UnitActionData.ParseType:type_name -> ServerCommon.ActionParseType
 	1, // 3: ServerCommon.InteractiveObj.status:type_name -> ServerCommon.InterObjStatus
 	3, // 4: ServerCommon.RegisterToGateway.serverType:type_name -> ServerCommon.ServerCategory
@@ -2031,7 +2085,7 @@ func file_sever_common_proto_init() {
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_sever_common_proto_rawDesc,
-			NumEnums:      4,
+			NumEnums:      5,
 			NumMessages:   13,
 			NumExtensions: 0,
 			NumServices:   0,
