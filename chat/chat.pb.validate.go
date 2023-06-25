79024b19a857758dba93a3c4e4f5bdaa6fb9158a
calvin <hhaou@outlook.com>
2023-06-25 15:41:35 +0800

    adding

--- chat/chat.pb.validate.go
+++ chat/chat.pb.validate.go
@@ -459,3 +459,82 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = SCCrossSrvChatMsgValidationError{}
+
+// Validate checks the field values on SCSystemMessage with the rules defined
+// in the proto definition for this message. If any rules are violated, an
+// error is returned.
+func (m *SCSystemMessage) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Content
+
+	// no validation rules for MsgType
+
+	// no validation rules for SendTime
+
+	// no validation rules for Language
+
+	// no validation rules for Paras
+
+	// no validation rules for Modules
+
+	// no validation rules for Channels
+
+	return nil
+}
+
+// SCSystemMessageValidationError is the validation error returned by
+// SCSystemMessage.Validate if the designated constraints aren't met.
+type SCSystemMessageValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SCSystemMessageValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SCSystemMessageValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SCSystemMessageValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SCSystemMessageValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SCSystemMessageValidationError) ErrorName() string { return "SCSystemMessageValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SCSystemMessageValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSCSystemMessage.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SCSystemMessageValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SCSystemMessageValidationError{}
