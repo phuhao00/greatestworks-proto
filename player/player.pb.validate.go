5b84c4c3775275e74c3e746b4752f78b3a4aae68
calvin <hhaou@outlook.com>
2023-06-26 15:49:50 +0800

    adding

--- player/player.pb.validate.go
+++ player/player.pb.validate.go
@@ -991,3 +991,83 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = PlayerSyncDataValidationError{}
+
+// Validate checks the field values on PlayerData with the rules defined in the
+// proto definition for this message. If any rules are violated, an error is returned.
+func (m *PlayerData) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for PlayerID
+
+	// no validation rules for GatewayID
+
+	// no validation rules for GatewayIp
+
+	// no validation rules for OpType
+
+	// no validation rules for RemoteIp
+
+	// no validation rules for Kick
+
+	// no validation rules for Reason
+
+	// no validation rules for Version
+
+	return nil
+}
+
+// PlayerDataValidationError is the validation error returned by
+// PlayerData.Validate if the designated constraints aren't met.
+type PlayerDataValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e PlayerDataValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e PlayerDataValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e PlayerDataValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e PlayerDataValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e PlayerDataValidationError) ErrorName() string { return "PlayerDataValidationError" }
+
+// Error satisfies the builtin error interface
+func (e PlayerDataValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sPlayerData.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = PlayerDataValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = PlayerDataValidationError{}
